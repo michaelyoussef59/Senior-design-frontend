9fe8b4124279f4730daeba39ea449e2c4513d2d8
michaelyoussef59 <michaelyoussef59@gmail.com>
2024-01-22 16:14:02 -0700

    Added more functionality to users page buttons, no api functionality yet

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  searchUsersTextAreaValue: string = ''; // string taken from user search field
   firstNameTextAreaValue: string = ''; // string taken from first name field
   lastNameTextAreaValue: string = ''; // string taken from last name field
   credentialNumberTextAreaValue: string = ''; // string taken from credential number field
@@ -13,15 +14,27 @@ export class UsersComponent {
   activationDateTextAreaValue: string = ''; // string taken from activation date field
   expirationDateTextAreaValue: string = ''; // string taken from expiration date field
 
+  clearEnteredUserData() {
+    //This function clears all of the fields populated by the user
+    this.firstNameTextAreaValue = ''; // string taken from first name field
+    this.lastNameTextAreaValue = ''; // string taken from last name field
+    this.credentialNumberTextAreaValue = ''; // string taken from credential number field
+    this.accessLevelTextAreaValue  = ''; // string taken from access level field
+    this.activationDateTextAreaValue = ''; // string taken from activation date field
+    this.expirationDateTextAreaValue = ''; // string taken from expiration date field
+  }
+
   onSaveClick() {
     console.log("Save clicked: ", this.firstNameTextAreaValue, this.lastNameTextAreaValue, this.credentialNumberTextAreaValue, this.accessLevelTextAreaValue, this.activationDateTextAreaValue, this.expirationDateTextAreaValue);
+    this.clearEnteredUserData();
   }
 
   onCancelClick() {
     console.log("Cancel clicked");
+    this.clearEnteredUserData();
   }
 
   onSearchClick() {
-    console.log("Search clicked");
+    console.log("Search clicked", this.searchUsersTextAreaValue);
   }
 }
