2119fb7d5ce23e4f238558a5a4976080b6778776
michaelyoussef59 <michaelyoussef59@gmail.com>
2024-01-22 23:21:25 -0700

    Added some functionality to access points, no api yet

--- src/app/accesspoints/accesspoints.component.ts
+++ src/app/accesspoints/accesspoints.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./accesspoints.component.css']
 })
 export class AccesspointsComponent {
+  searchFacilitiesUserEnteredTextArea: string = ''; // to be populated by user
+  searchAccessPointsUserEnteredTextArea: string = ''; // to be populated by user
+  facilitiesResult: string = ''; // to be populated by init or search
+
+  ngOnInit() {
+    // populate facilities from api here
+    this.facilitiesResult = '';
+  }
+
+  onSearchFacilitiesButtonClick() {
+    console.log("Search facilities button clicked", this.searchFacilitiesUserEnteredTextArea);
+  }
+
+  onSearchAccessPointsButtonClick() {
+    console.log("Search access points button clicked", this.searchAccessPointsUserEnteredTextArea);
+
+  }
+
 
 }
